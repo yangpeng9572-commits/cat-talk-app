886ee9e2662fcfbba1f3d3347eac1591811a51f0
小龍女 <dragon@cat-talk.app>
2026-05-06 19:57:56 +0800

    fix(theme): use CardTheme for Flutter 3.24 compatibility

--- lib/theme/kawaii_theme.dart
+++ lib/theme/kawaii_theme.dart
@@ -128,7 +128,7 @@ class KawaiiTheme {
     ),
     
     // 卡片
-    cardTheme: CardThemeData(
+    cardTheme: CardTheme(
       color: cardBackground,
       elevation: 0,
       shape: RoundedRectangleBorder(
